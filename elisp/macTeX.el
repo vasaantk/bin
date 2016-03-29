9dbaef4ec3f1d1cb37dc19718abc127b4859c98a
Vasaant Krishnan <vasaantk@gmail.com>
2016-03-29 16:57:37 +0200

    Removed obsolete python scripts.

--- elisp/macTeX.el
+++ elisp/macTeX.el
@@ -12,17 +12,6 @@
 (add-to-list 'exec-path "/Library/TeX/texbin/")
 
 (require 'tex-site)
-;; AucTeX
-;; (setq TeX-auto-save t)
-;; (setq TeX-parse-self t)
-;; (setq-default TeX-master nil)
-;; (add-hook 'LaTeX-mode-hook 'longlines-mode)
-;; (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-;; (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-;; (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-;; (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-;; (setq reftex-plug-into-AUCTeX t)
-;; (setq TeX-PDF-mode t)
 
 ;; Use Skim as viewer, enable source <-> PDF sync
 ;; make latexmk available via C-c C-c
