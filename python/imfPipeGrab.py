00bb6ffd78d1a56f6ec6ffa3f21f2e92dadf8350
Vasaant Krishnan <vasaantk@gmail.com>
2016-08-05 15:25:04 +0200

    Changed the print order of imfPipeGrab.py

--- python/imfPipeGrab.py
+++ python/imfPipeGrab.py
@@ -28,13 +28,13 @@ for line in sys.stdin:
         if loopkeeper:
             loopkeeper = False
             print ""
-            print " %12s %12s %12s %12s %12s  %s"%("Flux","xOff","xErr","yOff","yErr","Comments:")
+            print " %12s %12s %12s %12s %12s  %s"%("xOff","xErr","yOff","yErr","Flux","Comments:")
         flux = float(reqInfo.group(2))
         xOff = float(reqInfo.group(4))
         xErr = float(reqInfo.group(5))
         yOff = float(reqInfo.group(6))
         yErr = float(reqInfo.group(7))
         note = str(  reqInfo.group(21))
-        print " %12.3f %12.3f %12.3f %12.3f %12.3f %s"%(flux,xOff,xErr,yOff,yErr,note)
+        print " %12.3f %12.3f %12.3f %12.3f %12.3f %s"%(xOff,xErr,yOff,yErr,flux,note)
 if reqInfo:
     print ""
