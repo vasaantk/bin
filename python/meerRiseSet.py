e771b288ccd9adffccc7aafe51a1df7206712be5
Vasaant Krishnan <vasaantk@gmail.com>
2018-09-23 17:42:49 +0200

    Big changes to meerRiseSet.py
    These include ability to use your own time and further refinements to
    the code.

--- python/meerRiseSet.py
+++ python/meerRiseSet.py
@@ -1,14 +1,25 @@
 #! /usr/bin/env python
 
+# IMPORTANT: All times are in UTC.
+#
 # meerRiseSet.py is based on Lindsay's
 # "Scheduling_catalogue_tester.ipynb" to compute source rise, transit
-# and set times. Check the "Antenna parameters".
+# and set times. The input "source_catalogue.csv" is of the format in
+# katsdpcatalogue files.
+#
+# If no date and time is given, current UTC time is used.
+# Acceptable time formats:
+
+#            2018-09-29 14:23:45.3240
+#            2018/09/29 14:23:45.3240
+#            2018-09-29 14:23
+#
+# Usage:
+#        -->$ cat source_catalogue.csv | meerRiseSet.py -p 2018-09-29 14:23:45.3240
+#
+# -p = plot the trajectories
 
 # Written by Vasaant S/O Krishnan on Saturday, 22 September 2018
-
-# Usage:
-#        -->$ sourceCatalogue.csv | meerRiseSet.py -p
-#   -p flag is to plot the trajectories.
 
 import katpoint
 import numpy as np
@@ -23,72 +34,89 @@ import sys
 
 #======================================================================
 #    Setup some variables
+refAnt = katpoint.Antenna('m000, -30.71292524, 21.44380306, 1035')    # the MeerKAT reference point
+refAnt.ref_observer.horizon = '20:00:00'                              # horizon set to 20 degrees
 
-ant    = katpoint.Antenna('m000, -30.71292524, 21.44380306, 1035')     # the MeerKAT reference point
-ant.ref_observer.horizon = '20:00:00'                                  # horizon set to 20 degrees
 
-priFlag  = True           # Flag to allow first 'target' as primary calibrator.
 
-cat = katpoint.Catalogue()
-cat.antenna = ant
-
+#======================================================================
+#    Harvest user defined variables and process
 usrInp   = sys.argv[1:]
-plotFlag = False
+dateFlag = False        # Assume no user date
+timeFlag = False        # Assume no user time
+priFlag  = True         # Make first 'target' source primary
+plotFlag = False        # Assume plot is not requested
+
 if '-p' in usrInp:
     plotFlag = True
 
+usrInp = [i.replace(':',' ').replace('-',' ').replace('/',' ') for i in usrInp]
 
+for i in usrInp:
+    if re.match('\d\d\d\d \d\d \d\d', i):
+        usrDate  = i.replace(' ','-')
+        dateFlag = True
+    if re.match('\d\d \d\d[ \d\d.\d+]*', i):
+        usrTime  = i.replace(' ',':')
+        timeFlag = True
+
+if (not dateFlag or not timeFlag) and len(usrInp) >= 1:
+    print "Error in input time. Reverting to current UTC time"
+    print ""
+
+if dateFlag and timeFlag:
+    startTimeStamp = katpoint.Timestamp(usrDate + ' ' + usrTime)
+else:
+    startTimeStamp = katpoint.Timestamp()
+startEphemTime     = startTimeStamp.to_ephem_date()
+
+
+
+#======================================================================
+#    Code begins here
+cat = katpoint.Catalogue()
+cat.antenna = refAnt
 
 for line in sys.stdin:    # Harvest source info from the input catalogue
     if line[0] != '#':
         cat.add(line)
 
-start_timestamp = katpoint.Timestamp()            # use NOW as the start time for the obs table
-start_ed        = start_timestamp.to_ephem_date()
-
-# Setup table header:
+# Setup table header
 print "%10s%20s%20s%20s"%('Target','Next Rise (UTC)', 'Next Transit', 'Next Set')
 print 70*"-"
 
 # Compute rise, transit & set times and populate table
 for tar in cat.targets:
     try:
-        rise_time = str((ant.ref_observer.next_rising(tar.body,start_ed).datetime()))[:16]
+        riseTime = str((refAnt.ref_observer.next_rising( tar.body,startEphemTime).datetime()))[:16]
     except:
-        rise_time = 'source does not rise'
+        riseTime = 'No rise'
     try:
-        transit_time = str((ant.ref_observer.next_transit(tar.body,start_ed).datetime()))[:16]
+        tranTime = str((refAnt.ref_observer.next_transit(tar.body,startEphemTime).datetime()))[:16]
     except:
-        transit_time = 'source does not transit'
+        tranTime = 'No transit'
     try:
-        set_time = str((ant.ref_observer.next_setting(tar.body,start_ed).datetime()))[:16]
+        setTime  = str((refAnt.ref_observer.next_setting(tar.body,startEphemTime).datetime()))[:16]
     except:
-        set_time = 'source does not set'
+        setTime  = 'No set'
 
-    tags = str([i for i in tar.tags if i != 'radec'])
-    tags = tags.replace("[","")
-    tags = tags.replace("]","")
-    tags = tags.replace("'","")
+    tags = str([i for i in tar.tags if i != 'radec']).replace("[","").replace("]","").replace("'","")
 
-    if 'target' in tar.tags and priFlag:    # Select first 'target' as primary target
+    if 'target' in tar.tags and priFlag:
         priTarg = tar
         priFlag = False
 
-    print "%10s%20s%20s%20s   %s"%(str(tar.name), str(rise_time), str(transit_time), str(set_time), tags)
+    print "%10s%20s%20s%20s   %s"%(str(tar.name), str(riseTime), str(tranTime), str(setTime), tags)
 print ""
 
-
-
-if priFlag:     # Manually assign primary target incase no 'target' sources in cat.targets
+if priFlag:     # Manually assign primary target if no 'target' found in cat.targets
     priTarg = tar
 
 
 
 #======================================================================
 #    Plot parameters
-xlow = katpoint.Timestamp()
-xhig = katpoint.Timestamp() + 24*60*60
-t    = start_timestamp.secs + np.arange(0, 24. * 60. * 60., 360.)
+t    = startTimeStamp.secs + np.arange(0, 24. * 60. * 60., 360.)
 lst  = katpoint.rad2deg(priTarg.antenna.local_sidereal_time(t)) / 15
 fig, ax1 = plt.subplots()
 
@@ -104,7 +132,7 @@ for target in cat.targets:
     labels.append(target.name)
     lst_rise = lst[np.where(elev>20)[0][ 0]]
     lst_set  = lst[np.where(elev>20)[0][-1]]
-    print('%15s is above 20 degrees between LST %05.02f and %05.02f '%(target.name, lst_rise, lst_set))
+    # print "%15s is above 20 degrees between LST %05.02f and %05.02f"%(target.name, lst_rise, lst_set)
 
 ax1.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
 ax1.xaxis.set_major_locator(mdates.HourLocator(byhour=range(24),interval=1))
@@ -115,7 +143,7 @@ plt.ylim(20,90)
 plt.grid()
 plt.legend()
 plt.ylabel('Elevation (deg)')
-plt.xlabel ('Time (UTC) starting from %s'%start_timestamp.to_string())
+plt.xlabel('UTC time starting from %s'%startTimeStamp.to_string())
 ax2 = ax1.twiny()
 ax2.xaxis.set_major_locator(MaxNLocator(24))
 minorLocator = MultipleLocator(0.25)
